91063de6fd582c49e5efe2f1a41f64a73be0ae2e
Jalan Neal <60294732+jalanmneal00@users.noreply.github.com>
2023-03-31 11:20:00 -0400

    Add files via upload
    Guessing game/Thinking of a number between 1 and 100

---
+++ Lab3.cpp
@@ -0,0 +1,28 @@
+/*Program by Jalan Neal*/
+#include <iostream>
+using namespace std;
+#include <stdlib.h>
+#include <cmath>;
+int target = rand() % 100 + 1; // number to be guessed
+
+int main() {
+	double Guess = 0; // random number
+	srand(time(NULL)); // seed the random number generator
+	int target = rand() % 100 + 1; // number to be guessed
+	int guesses = 0;//number of guesses
+while (guess!= random) {
+	cout << "Enter a guess from 1 to 100" << endl;// the results of the input //
+	cin >> random;
+	guesses = guesses++;
+	if (Guess > 47) {
+		cout << "Too Low" << endl;
+	}
+	else (Guess > 77) {
+		cout << "Too Low" << endl;
+	else if(Guess > 88){
+		cout << "Too High" << endl;
+	else if(Guess > 82){
+		cout << "Too High" << endl;
+	else if (Guess > 79) {
+	cout << "Correct in 5 guesses!" << endl;
+}
